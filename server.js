e76f2e8bd2b32117f62e185ffd00ff2e7c8e0249
kamichchha <kamichchha@gmail.com>
2016-07-26 19:58:55 +0530

    Changed server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/about',middleware.requestAuth,function(req,res){
-	res.send('About Us!');
+	res.send('About Us....');
 });
 
 app.use(express.static(__dirname+'/public'));
